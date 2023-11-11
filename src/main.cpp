bf3659d3b4a918dd003a287e9160b33e650720c2
michang <minseok128128@gmail.com>
2023-11-11 16:23:34 +0900

    refactor: rename

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
-#include "LexicalAnalyzer.h"
+#include "Lexer.h"
 #include "Parser.h"
 
 int main(int argc, char **argv) {
-    if (!LexicalAnalyzer::initializeInstance(argv[1]))
+    if (!Lexer::initializeInstance(argv[1]))
         return (1);
     Parser::getInstance()->program();
     return (0);
